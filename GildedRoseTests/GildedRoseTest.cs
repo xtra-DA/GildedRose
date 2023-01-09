04497f3a013f8b488213552c47b71dc4f335eef6
timmy.gilissen <timmy.gilissen@gilit.be>
2023-01-09 15:04:53 +0100

    first commit

---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,16 @@
+using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void Foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("fixme", Items[0].Name);
+    }
+}
